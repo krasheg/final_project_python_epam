caeb5a78f7245e63351e1846ae8f2604f70d66a5
INlOEtdF <krasheg@gmail.com>
2022-01-15 22:15:40 +0200

    update travis settings

--- department_app/views/__init__.py
+++ department_app/views/__init__.py
@@ -1,3 +1,5 @@
+"""Views package that contains all views for department, employee and homepage pages"""
+# pylint: disable=wrong-import-position, protected-access, cyclic-import
 from . import employee_view
 from . import department_view
 from . import index_view
