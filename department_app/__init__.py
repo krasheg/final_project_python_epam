2b482aaa3d5b2d5b05476bf4558b51d087fe1aad
INlOEtdF <krasheg@gmail.com>
2022-01-15 18:08:08 +0200

    add logging

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,8 +1,10 @@
+import os
+import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
-
+import logging.config
 from config import Config
 
 app = Flask(__name__)
@@ -34,3 +36,26 @@ api.add_resource(department_api.DepartmentApi, '/api/departments/<id>')
 api.add_resource(employee_api.EmployeeListApi, '/api/employees')
 api.add_resource(employee_api.EmployeeApi, '/api/employees/<id>')
 api.add_resource(employee_api.EmployeeSearchApi, '/api/employees/search')
+
+# logging
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+
+file_handler = logging.FileHandler(filename='app.log', mode='w')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+
+logger = app.logger
+logger.handlers.clear()
+app.logger.addHandler(file_handler)
+app.logger.addHandler(console_handler)
+app.logger.setLevel(logging.DEBUG)
+
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.handlers.clear()
+werkzeug_logger.addHandler(file_handler)
+werkzeug_logger.addHandler(console_handler)
+werkzeug_logger.setLevel(logging.DEBUG)
